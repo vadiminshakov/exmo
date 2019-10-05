7382c2afdd977c94601145f60b520f87f2b16be0
Vadim Inshakov <vadiminshakov@gmail.com>
2019-10-05 16:01:20 +0300

    Change ApiParams map value to interface{}

--- exmo.go
+++ exmo.go
@@ -23,7 +23,7 @@ import (
 type ApiResponse map[string]interface{}
 
 // ApiParams is a map for API calls' params
-type ApiParams map[string]string
+type ApiParams map[string]interface{}
 
 // Exmo holds client-specific info
 type Exmo struct {
